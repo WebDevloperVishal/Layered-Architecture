520412ea949daebc0b6e58d75c021333edb11fb6
WebDevloperVisha <vishalsanam2024@gmail.com>
2025-12-04 12:22:55 +0530

    Commit

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,23 @@
+const { version } = require("mongoose");
+
 require("dotenv").config();
 
 const config = {
     port: process.env.PORT || 8080,
 
-    
+    mongodb: {
+        uri: process.env.MONGODB_URL,
+    },
+
+    api: {
+        prefix: "/api",
+        version: "v1"
+    },
+
+
+    cors: {
+        origin: process.env.CORS_ORIGIN || '*',
+        Credentials: true
+    }
+
 }
