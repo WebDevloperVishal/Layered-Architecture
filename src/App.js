8f55919669e95e276bde9d0ce823c1568eef5cf9
WebDevloperVisha <vishalsanam2024@gmail.com>
2025-12-04 12:59:28 +0530

    Commit

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ app.use(cors({
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 }))
 
+
 app.use(express.json({ limit: '10mb'}));
 app.use(express.urlencoded({ extended: true, limit: '10mb'}));
 
--- src/config/index.js
+++ src/config/index.js
@@ -21,3 +21,5 @@ const config = {
     }
 
 }
+
+module.exports = config
