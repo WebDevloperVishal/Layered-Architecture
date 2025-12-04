e057c017f8202bf9e04d7bb26979faab55de3a6d
WebDevloperVisha <vishalsanam2024@gmail.com>
2025-12-04 12:30:56 +0530

    Commit

--- src/App.js
+++ src/App.js
@@ -13,3 +13,10 @@ app.use(cors({
 
 app.use(express.json({ limit: '10mb'}));
 app.use(express.urlencoded({ extended: true, limit: '10mb'}));
+
+app.get("/", (req,res) => {
+    res.json({
+        message: "Layered_Architecture",
+        success: true
+    })
+})
