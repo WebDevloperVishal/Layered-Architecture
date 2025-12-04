eba0346fb255ed33cbb114d32ab4a6fe5d8a78fb
WebDevloperVisha <vishalsanam2024@gmail.com>
2025-12-04 12:27:14 +0530

    Commit

--- src/App.js
+++ src/App.js
@@ -2,3 +2,12 @@ const express = require("express");
 const cors = require("cors");
 
 const config = require("./config");
+
+const app = express();
+
+app.use(cors({
+    origin: config.cors.origin,
+    credentials: config.cors.credentials,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+}))
+
